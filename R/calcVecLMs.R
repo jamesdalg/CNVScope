340027a94786ec32d75d792436d8d7207dd57ddd
Dalgleish <dalgleishjl@nih.gov>
2018-08-13 09:58:45 -0400

    Fixed calcVecLMs and added a new function to get the negative log pvalue, handling edge cases. This will allow greater replicatability and will make the results more seamless. Please note-- you

--- R/calcVecLMs.R
+++ R/calcVecLMs.R
@@ -14,20 +14,14 @@
 #' #full_matrix<-calcVecLMs(bin_data)
 #'}
 #' @export
-calcVecLMs<-function(bin_data,use_slurm=F,job_finished=F,slurmjob=NULL,n_nodes=NULL,cpus_on_each_node=1,memory_per_node="2g",walltime="4:00:00",n_cores=1)
+calcVecLMs<-function(bin_data,use_slurm=F,job_finished=F,slurmjob=NULL,n_nodes=NULL,cpus_on_each_node=2,memory_per_node="2g",walltime="4:00:00")
 {
   #if(dim(bin_data)[1]<dim(bin_data)[2]){bin_data<-t(bin_data)}
   bin_data_df<-as.data.frame(bin_data)
   bin.pairs<-expand.grid(1:ncol(bin_data),1:ncol(bin_data))
   colnames(bin.pairs)<-c("x","y")
   if(!use_slurm){
-    if(Sys.info()['sysname']!="Windows") {
-      ncores=1
-    } else {ncores=n_cores}
-    if(Sys.info()['sysname']!="Windows")    { 
-      neglogpvalues<-mcmapply(mc.cores = ncores,x=bin.pairs[,1],y=bin.pairs[,2],FUN = function(x,y) {lmsum<-summary(lm(unlist(bin_data_df[,y])~unlist(bin_data_df[,x]))); return(-log(lmsum$coefficients[2,4])*sign(lmsum$coefficients[2,1])) } ) } else {
-      neglogpvalues<-mapply(x=bin.pairs[,1],y=bin.pairs[,2],FUN = function(x,y) {lmsum<-summary(lm(unlist(bin_data_df[,y])~unlist(bin_data_df[,x]))); return(-log(lmsum$coefficients[2,4])*sign(lmsum$coefficients[2,1])) } )
-    }
+    neglogpvalues<-mcmapply(x=bin.pairs[,1],y=bin.pairs[,2],function(x,y) -log(summary(lm(unlist(bin_data_df[,y])~unlist(bin_data_df[,x])))$coefficients[2,4]) )
     output_matrix<-matrix(neglogpvalues,ncol=ncol(bin_data))
     output_matrix[is.infinite(output_matrix)]<-max(output_matrix[!is.infinite(output_matrix)])
     return(output_matrix)
@@ -42,7 +36,7 @@ calcVecLMs<-function(bin_data,use_slurm=F,job_finished=F,slurmjob=NULL,n_nodes=N
       return(output_matrix)
     } else {
       if(is.null(n_nodes)){n_nodes<-ncol(bin_data_df)/2}
-      lm_test_sjob <- slurm_apply(f = function(x,y) {lmsum<-summary(lm(unlist(bin_data_df[,y])~unlist(bin_data_df[,x]))); return(-log(lmsum$coefficients[2,4])*sign(lmsum$coefficients[2,1])) },params =  bin.pairs, jobname = 'test_apply',
+      lm_test_sjob <- slurm_apply(function(x,y) -log(summary(lm(unlist(bin_data_df[,y])~unlist(bin_data_df[,x])))$coefficients[2,4]), bin.pairs, jobname = 'test_apply',
                                   nodes = n_nodes, cpus_per_node = cpus_on_each_node, submit = T,slurm_options = list(partition="ccr,norm,quick",mem=memory_per_node,time=walltime,cpus_per_task=cpus_on_each_node))
       return(lm_test_sjob)
     }
