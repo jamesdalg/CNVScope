8dca9d29cd67acfc1d3cac51f11dcb74f946e49d
Dalgleish <dalgleishjl@nih.gov>
2018-08-21 10:23:27 -0400

    fixed hicnv shiny components

--- inst/doc/create_output_matrix.R
+++ inst/doc/create_output_matrix.R
@@ -17,14 +17,17 @@ nbl_input_matrix[1:5,1:5]
 
 ## ---- eval=F-------------------------------------------------------------
 #  library(parallel)
-#  nbl_slurm_object_test_zero_removed<-calcVecLMs(bin_data = as.data.frame(t(nbl_input_matrix))[,colSums(as.data.frame(t(nbl_input_matrix)))>0],use_slurm = T,n_nodes = 975,memory_per_node = "32g",walltime = "04:00:00",n_cores = 2,cpus_on_each_node = 2,job_finished = F,slurmjob = NULL)
+#  nbl_slurm_object_test_zero_removed<-calcVecLMs(bin_data =as.data.frame(t(nbl_input_matrix[which(rowSds(as.matrix(nbl_input_matrix))!=0.0),])),use_slurm = T,n_nodes = 975,memory_per_node = "32g",walltime = "04:00:00",cpus_on_each_node = 2,job_finished = F,slurmjob = NULL)
+#  
 
 ## ---- eval=F-------------------------------------------------------------
 #  
 #  saveRDS(nbl_slurm_object_test_zero_removed,"nbl_slurm_object_test_zero_removed.rds")
 
 ## ---- eval=F-------------------------------------------------------------
-#  nbl_result_matrix<-matrix(get_slurm_out(nbl_slurm_object_test_zero_removed),ncol=ncol(as.data.frame(t(nbl_input_matrix))[,colSums(as.data.frame(t(nbl_input_matrix)))>0]))
+#  library(matrixStats)
+#  nbl_result_matrix<-matrix(as.numeric(unlist( get_slurm_out(nbl_slurm_object_test_zero_removed))),ncol=ncol(as.data.frame(t(nbl_input_matrix[which(rowSds(as.matrix(nbl_input_matrix))!=0.0),])) ) )
+#  
 #  saveRDS(nbl_result_matrix,"nbl_result_matrix_full.rds")
 #  
 
