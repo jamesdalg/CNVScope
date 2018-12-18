b395bd6af059ff10b5225a405d20f691f4cd2f05
Dalgleish <dalgleishjl@nih.gov>
2018-12-18 00:32:59 -0500

    TCGA read file support fixed for readGDCfile

--- R/freadGDCfile.R
+++ R/freadGDCfile.R
@@ -8,6 +8,7 @@
 #' @importFrom dplyr bind_cols
 #' @param file GDC file to be read
 #' @param fread_skip The number of metadata lines to be skipped(typically 14)..
+#' @references https://docs.gdc.cancer.gov/Encyclopedia/pages/TCGA_Barcode/
 #' @return input_tsv_with_sample_info A data frame containing the sample information extracted
 #'  from the filename, including sample name & comparison type.
 #' @examples 
@@ -15,9 +16,22 @@
 #' system.file("extdata","somaticCnvSegmentsDiploidBeta_TARGET-30-PANRVJ_NormalVsPrimary.tsv",
 #' package = "CNVScope"))
 #' @export
-freadGDCfile<-function(file,fread_skip=14) {input_tsv<-data.table::fread(file,skip=fread_skip)
+freadGDCfile<-function(file,fread_skip=14, format = "TARGET") {
+if(format=="TARGET"){
+input_tsv<-data.table::fread(file,skip=fread_skip)
 sample_info_colsplit<-reshape2::colsplit(basename(file),"_|-|\\.",c("pre","project","num","sample","comparison","fn_ext"))
 input_tsv_with_sample_info<-dplyr::bind_cols(input_tsv,sample_info_colsplit[rep(1,nrow(input_tsv)),])
 if(length(na.omit(unlist(sample_info_colsplit)))!=6){return(NULL)}
+}
+if(format=="TCGA")
+{
+  input_tsv_with_sample_info<-data.table::fread(file,skip=fread_skip) %>% 
+  dplyr::mutate(fn = basename(file),sep="--") %>% 
+ tidyr::separate(fn,sep="---", into = c("barcode1","barcode2","dataformat")) %>% 
+ tidyr::separate(barcode1,sep="-|_", into = c("project1","tss1","participant1","sample1","portion_analyte1","plate1","center1","barcode_id1"),remove=FALSE) %>%
+ tidyr::separate(barcode2,sep="-|_", into = c("project2","tss2","participant2","sample2","portion_analyte2","plate2","center2","barcode_id2"),remove=FALSE) %>%
+    dplyr::select( -sep)
+}
 return(input_tsv_with_sample_info)
 }
+#for reference, see TCGA barcode documentation.
