4bb0b83ff09731f2072a48727d065ba4474a5695
Dalgleish <dalgleishjl@nih.gov>
2018-12-19 10:05:54 -0500

    updated freadGDCfile to skip the correct number of lines for each format

--- R/freadGDCfile.R
+++ R/freadGDCfile.R
@@ -16,8 +16,9 @@
 #' system.file("extdata","somaticCnvSegmentsDiploidBeta_TARGET-30-PANRVJ_NormalVsPrimary.tsv",
 #' package = "CNVScope"))
 #' @export
-freadGDCfile<-function(file,fread_skip=14, format = "TARGET") {
+freadGDCfile<-function(file,fread_skip=NULL, format = "TARGET") {
 if(format=="TARGET"){
+if(is.null(fread_skip)){  fread_skip=14}
 input_tsv<-data.table::fread(file,skip=fread_skip)
 sample_info_colsplit<-reshape2::colsplit(basename(file),"_|-|\\.",c("pre","project","num","sample","comparison","fn_ext"))
 input_tsv_with_sample_info<-dplyr::bind_cols(input_tsv,sample_info_colsplit[rep(1,nrow(input_tsv)),])
@@ -25,11 +26,13 @@ if(length(na.omit(unlist(sample_info_colsplit)))!=6){return(NULL)}
 }
 if(format=="TCGA")
 {
+  if(is.null(fread_skip)){  fread_skip=0}
   input_tsv_with_sample_info<-data.table::fread(file,skip=fread_skip) %>% 
   dplyr::mutate(fn = basename(file),sep="--") %>% 
  tidyr::separate(fn,sep="---", into = c("barcode1","barcode2","dataformat")) %>% 
  tidyr::separate(barcode1,sep="-|_", into = c("project1","tss1","participant1","sample1","portion_analyte1","plate1","center1","barcode_id1"),remove=FALSE) %>%
  tidyr::separate(barcode2,sep="-|_", into = c("project2","tss2","participant2","sample2","portion_analyte2","plate2","center2","barcode_id2"),remove=FALSE) %>%
+    dplyr::mutate( uuid = dirname(file) %>% tibble::as.tibble() %>% tidyr::separate(value, sep="/",into=c("dir","uuid")) %>% dplyr::pull(uuid)) %>% 
     dplyr::select( -sep)
 }
 return(input_tsv_with_sample_info)
