0c1f04f6bb094e7afa3849b13cabe4c22f02c944
jamesdalg <james.dalgleish@nih.gov>
2019-09-21 14:20:21 -0400

    commit rebuilt vignettes

--- vignettes/create_input_matrix.R
+++ vignettes/create_input_matrix.R
@@ -19,3 +19,27 @@ library(CNVScope)
 #  saveRDS(sample_aggregated_segvals_output_full,"NBL_sample_matched_input_matrix.rds")
 #  
 
+## ---- eval=F,echo=T------------------------------------------------------
+#  nbl_custom_input_matrix<-formSampleMatrixFromRawGDCData(tcga_files = target_files_nbl,
+#  format = "custom",binsize = 1e6,freadskip = 14,parallel=F,debug=F,
+#  sample_pat = "(?<=30-)(.*?)(?=_)",sample_col = "sample",chrlabel=">chr",
+#  startlabel = "begin",endlabel = "end",cnlabel = "relativeCvg")
+#  saveRDS(nbl_custom_input_matrix,"NBL_custom_sample_matched_input_matrix.rds")
+#  
+
+## ---- echo=T,eval=F------------------------------------------------------
+#  nbl_custom_input_matrix_hd<-formSampleMatrixFromRawGDCData(tcga_files = target_files_nbl,
+#  format = "custom",binsize = 2.5e5,freadskip = 14,parallel=F,debug=F,
+#  sample_pat = "(?<=30-)(.*?)(?=_)",sample_col = "sample",chrlabel=">chr",
+#  startlabel = "begin",endlabel = "end",cnlabel = "relativeCvg")
+#  saveRDS(nbl_custom_input_matrix_hd,"NBL_custom_sample_matched_input_matrix_hd.rds")
+#  
+
+## ---- echo=T,eval=F------------------------------------------------------
+#  nbl_custom_input_matrix_ld<-formSampleMatrixFromRawGDCData(tcga_files = target_files_nbl,
+#  format = "custom",binsize = 1e8,freadskip = 14,parallel=F,debug=F,
+#  sample_pat = "(?<=30-)(.*?)(?=_)",sample_col = "sample",chrlabel=">chr",
+#  startlabel = "begin",endlabel = "end",cnlabel = "relativeCvg")
+#  saveRDS(nbl_custom_input_matrix,"NBL_custom_sample_matched_input_matrix_ld.rds")
+#  
+
