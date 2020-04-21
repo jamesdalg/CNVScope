d23d50f1dd5a254d32971f6f6b255f1d004b5bef
jamesdalg <james.dalgleish@nih.gov>
2020-04-20 20:28:20 -0400

    adding libgsl-dev dependency into install script

--- doc/additonal_examples.R
+++ doc/additonal_examples.R
@@ -1,4 +1,4 @@
-## ----setup, include=FALSE------------------------------------------------
+## ----setup, include=FALSE-----------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE)
 
 knitr::opts_knit$set(root.dir = '.')
@@ -6,22 +6,22 @@ library(CNVScope)
 options(scipen=999)
 library(magrittr)
 
-## ----aml_files,eval=F,echo=T---------------------------------------------
+## ----aml_files,eval=F,echo=T--------------------------------------------------
 #  if(!dir.exists("extracted_aml_data")){dir.create("extracted_aml_data")}
 #  untar("gdc_download_aml.tar.gz",exdir = "./extracted_aml_data")
 #  target_files_aml<-list.files(path = "extracted_aml_data",pattern=glob2rx("*NormalVsPrimary.tsv"),recursive=T,full.names = T)
 #  print(target_files_aml)
 
-## ----eval=F,echo=T-------------------------------------------------------
+## ----eval=F,echo=T------------------------------------------------------------
 #  sample_aggregated_segvals_aml<-formSampleMatrixFromRawGDCData(tcga_files = target_files_aml,format = "TARGET")
 #  saveRDS(sample_aggregated_segvals_aml,"aml_sample_matched_input_matrix.rds")
 
-## ----aml_plots, eval=T,echo=T--------------------------------------------
+## ----aml_plots, eval=T,echo=T-------------------------------------------------
 sample_aggregated_segvals_aml<-readRDS("aml_sample_matched_input_matrix.rds")
 invariant_bins<-which((sample_aggregated_segvals_aml[stringr::str_detect(rownames(sample_aggregated_segvals_aml),"chr7"),] %>% t() %>% as.data.frame() %>% sapply(sd))==0)
 chr_7_mat<-sample_aggregated_segvals_aml[(stringr::str_detect(rownames(sample_aggregated_segvals_aml),"chr7") & rownames(sample_aggregated_segvals_aml) %in% setdiff(rownames(sample_aggregated_segvals_aml),names(invariant_bins))),] %>% t()
 
-## ----chr7_cor------------------------------------------------------------
+## ----chr7_cor-----------------------------------------------------------------
 chr_7_mat %>%  cor(use="pairwise.complete.obs",method="pearson") %>% 
   CNVScope::signedRescale(max_cap=1) %>%
   reshape2::melt()  %>%
@@ -31,13 +31,13 @@ chr_7_mat %>%  cor(use="pairwise.complete.obs",method="pearson") %>%
   theme(axis.text.x = element_blank(),axis.text.y=element_blank(),axis.title = element_blank()) +
   ggplot2::scale_fill_gradient2(low = "blue", high = "red", midpoint = 0.5, limits = c(0, 1))
 
-## ----breakpoints---------------------------------------------------------
+## ----breakpoints--------------------------------------------------------------
 colnames(chr_7_mat)[CNVScope::getAsymmetricBlockIndices(cor(chr_7_mat,use="pairwise.complete.obs"))]
 breakpoints<-colnames(chr_7_mat)[CNVScope::getAsymmetricBlockIndices(cor(chr_7_mat,use="pairwise.complete.obs"))] %>% stringr::str_split_fixed(string = .,pattern="_",n=3) %>% as.matrix() %>% .[,2] %>% as.numeric()
 breakpoint_labels <- colnames(chr_7_mat)[CNVScope::getAsymmetricBlockIndices(cor(chr_7_mat,use="pairwise.complete.obs"))]
 breakpoint_labels
 
-## ----breakpoint_plot-----------------------------------------------------
+## ----breakpoint_plot----------------------------------------------------------
 chr_7_mat %>%  cor(use="pairwise.complete.obs",method="pearson") %>% 
     CNVScope::signedRescale(max_cap=1) %>%
     reshape2::melt()  %>%
@@ -49,13 +49,13 @@ chr_7_mat %>%  cor(use="pairwise.complete.obs",method="pearson") %>%
     ggplot2::scale_fill_gradient2(low = "blue", high = "red", midpoint = 0.5, limits = c(0, 1))
 
 
-## ----probdist------------------------------------------------------------
+## ----probdist-----------------------------------------------------------------
 chr_7_probdist <- CNVScope::calcCNVKernelProbDist(cor(chr_7_mat,use="pairwise.complete.obs"),parallel=F)$percentile_matrix
 js_breakpoints<-jointseg::jointSeg(chr_7_probdist,K=20)$bestBkp
 js_breakpoint_labels<-colnames(chr_7_mat)[js_breakpoints]
 
 
-## ----plot_probdist-------------------------------------------------------
+## ----plot_probdist------------------------------------------------------------
 chr_7_probdist %>%  
 #  CNVScope::signedRescale(max_cap=1) %>%
   reshape2::melt()  %>%
@@ -74,26 +74,26 @@ chr_7_probdist %>%
   ggplot2::scale_fill_gradient2(low = "blue", high = "red", midpoint = 0.5, limits = c(0, 1))
 
 
-## ----census_data,eval=F--------------------------------------------------
+## ----census_data,eval=F-------------------------------------------------------
 #  census_data <- readRDS(system.file("censushg19.rds",package = "CNVScope"))
 #  census_data[census_data@seqnames %in% "chr7"] %>% sort() %>% tibble::as_tibble() %>% janitor::clean_names() %>% dplyr::select(seqnames,start,end,gene_symbol,tumour_types_somatic,tumour_types_germline) %>% dplyr::filter(start>60e6,stringr::str_detect(string = tumour_types_somatic,pattern="AML") | stringr::str_detect(string = tumour_types_germline,pattern="AML"))
 
-## ----blca_files,eval=F,echo=T--------------------------------------------
+## ----blca_files,eval=F,echo=T-------------------------------------------------
 #  if(!dir.exists("extracted_blca_data")){dir.create("extracted_blca_data")
 #  untar("gdc_download_blca.tar.gz",exdir = "./extracted_blca_data")}
 #  tcga_files_blca<-list.files(path = "extracted_blca_data",pattern=glob2rx("*.tsv"),recursive=T,full.names = T)
 #  print(tcga_files_blca)
 
-## ----eval=F,echo=T-------------------------------------------------------
+## ----eval=F,echo=T------------------------------------------------------------
 #  sample_aggregated_segvals_blca<-formSampleMatrixFromRawGDCData(tcga_files = tcga_files_blca,format = "TCGA",parallel=T)
 #  saveRDS(sample_aggregated_segvals_blca,"blca_sample_matched_input_matrix.rds")
 
-## ----blca_plots, eval=T,echo=T-------------------------------------------
+## ----blca_plots, eval=T,echo=T------------------------------------------------
 sample_aggregated_segvals_blca<-readRDS("blca_sample_matched_input_matrix.rds")
 invariant_bins<-which((sample_aggregated_segvals_blca[stringr::str_detect(rownames(sample_aggregated_segvals_blca),"chr17"),] %>% t() %>% as.data.frame() %>% sapply(sd))==0)
 chr_17_mat<-sample_aggregated_segvals_blca[(stringr::str_detect(rownames(sample_aggregated_segvals_blca),"chr17") & rownames(sample_aggregated_segvals_blca) %in% setdiff(rownames(sample_aggregated_segvals_blca),names(invariant_bins))),] %>% t()
 
-## ----chr17_cor-----------------------------------------------------------
+## ----chr17_cor----------------------------------------------------------------
 chr_17_mat %>%  cor(use="pairwise.complete.obs",method="pearson") %>% 
   CNVScope::signedRescale(max_cap=1) %>%
   reshape2::melt()  %>%
@@ -103,7 +103,7 @@ chr_17_mat %>%  cor(use="pairwise.complete.obs",method="pearson") %>%
   theme(axis.text.x = element_blank(),axis.text.y=element_blank(),axis.title = element_blank()) +
   ggplot2::scale_fill_gradient2(low = "blue", high = "red", midpoint = 0.5, limits = c(0, 1))
 
-## ----probdist_chr17------------------------------------------------------
+## ----probdist_chr17-----------------------------------------------------------
 chr_17_probdist <- CNVScope::calcCNVKernelProbDist(cor(chr_17_mat,use="pairwise.complete.obs"),parallel=F)$percentile_matrix
 colnames(chr_17_probdist)<-colnames(chr_17_mat)
 rownames(chr_17_probdist)<-colnames(chr_17_mat)
@@ -112,7 +112,7 @@ chr_17_js_breakpoint_labels<-colnames(cor(chr_17_mat))[chr_17_js_breakpoints]
 chr_17_js_breakpoint_labels
 
 
-## ----breakpoint_plot_chr17,eval=F----------------------------------------
+## ----breakpoint_plot_chr17,eval=F---------------------------------------------
 #  
 #  breakpoint_plot_probdist <- chr_17_probdist %>% #  cor(use="pairwise.complete.obs",method="pearson") %>%
 #      CNVScope::signedRescale(max_cap=1) %>%
@@ -162,11 +162,11 @@ chr_17_js_breakpoint_labels
 #  breakpoint_plot_corr_diff
 #  
 
-## ----plotly_blca,eval=F--------------------------------------------------
+## ----plotly_blca,eval=F-------------------------------------------------------
 #  library(plotly)
 #  breakpoint_plot %>% plotly::ggplotly()
 
-## ----3D_blca,eval=F------------------------------------------------------
+## ----3D_blca,eval=F-----------------------------------------------------------
 #  chr_17_long <- chr_17_mat %>%   cor(use="pairwise.complete.obs",method="pearson") %>%
 #      CNVScope::signedRescale(max_cap=1) %>%
 #      reshape2::melt()  %>%
@@ -175,13 +175,13 @@ chr_17_js_breakpoint_labels
 #           correlation=value) %>% dplyr::select(col_pos,row_pos,correlation)
 #  plot_ly(data = chr_17_long, x=chr_17_long$col_pos,y=chr_17_long$row_pos,z=chr_17_long$correlation,color=c(0,0.5,1),colors=colorRamp(c("blue","white","red")),intensity=chr_17_long$correlation,type = "mesh3d")
 
-## ----skcm_files,eval=F,echo=T--------------------------------------------
+## ----skcm_files,eval=F,echo=T-------------------------------------------------
 #  if(!dir.exists("extracted_skcm_data")){dir.create("extracted_skcm_data")}
 #  untar("gdc_download_skcm.tar.gz",exdir = "./extracted_skcm_data")
 #  tcga_files_skcm<-list.files(path = "extracted_skcm_data",pattern=glob2rx("*.tsv"),recursive=T,full.names = T)
 #  print(tcga_files_skcm)
 
-## ----eval=F,echo=T-------------------------------------------------------
+## ----eval=F,echo=T------------------------------------------------------------
 #  #ptm <- proc.time()
 #  #doMC::registerDoMC()
 #  #doParallel::registerDoParallel()
@@ -189,14 +189,14 @@ chr_17_js_breakpoint_labels
 #  #proc.time() - ptm
 #  saveRDS(sample_aggregated_segvals_skcm,"skcm_sample_matched_input_matrix.rds")
 
-## ----eval=F,echo=T-------------------------------------------------------
+## ----eval=F,echo=T------------------------------------------------------------
 #  if(!dir.exists("extracted_prad_data")){dir.create("extracted_prad_data")
 #  untar("gdc_download_prad.tar.gz",exdir = "extracted_prad_data")}
 #  tcga_files_prad<-list.files(path = "extracted_prad_data",pattern=glob2rx("*.tsv"),recursive=T,full.names = T)
 #  print(tcga_files_prad)
 #  
 
-## ----eval=F,echo=T-------------------------------------------------------
+## ----eval=F,echo=T------------------------------------------------------------
 #  sample_aggregated_segvals_output_full_prad<-formSampleMatrixFromRawGDCData(tcga_files = tcga_files_prad,format = "TCGA",binsize=1e6)
 #  saveRDS(sample_aggregated_segvals_output_full_prad,"PRAD_sample_matched_input_matrix.rds")
 #  
