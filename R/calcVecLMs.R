bc958781b3896eca07e247588b57eb16dafc93f0
jamesdalg <james.dalgleish@nih.gov>
2021-06-23 12:16:54 -0400

    fixed rslurm issue with cpu_per_task in batch script.

--- R/calcVecLMs.R
+++ R/calcVecLMs.R
@@ -11,6 +11,8 @@
 #' @param cpus_on_each_node The number of cpus used on each node
 #' @param memory_per_node the amount of ram per node (e.g. "32g" or "2g")
 #' @param walltime Time for job to be completed for SLURM scheduler in hh:mm:ss format. Defaults to 4h.
+#' @param partitions the partitions to which the jobs are to be scheduled, in
+#' order of priority.
 #' @return The output matrix, or if using slurm, the slurm job object (which should be saved as an rds file and reloaded when creating the output matrix).
 #' @examples
 #'
@@ -24,7 +26,7 @@
 #' #the postProcessLinRegMatrix function (which converts the infinite values to a maximum).
 #'
 #' @export
-calcVecLMs<-function(bin_data,use_slurm=F,job_finished=F,slurmjob=NULL,n_nodes=NULL,cpus_on_each_node=2,memory_per_node="2g",walltime="4:00:00")
+calcVecLMs<-function(bin_data,use_slurm=F,job_finished=F,slurmjob=NULL,n_nodes=NULL,cpus_on_each_node=2,memory_per_node="2g",walltime="4:00:00",partitions="ccr,quick")
 {
   #if(dim(bin_data)[1]<dim(bin_data)[2]){bin_data<-t(bin_data)}
   #importFrom rslurm slurm_apply get_slurm_out
@@ -47,8 +49,10 @@ calcVecLMs<-function(bin_data,use_slurm=F,job_finished=F,slurmjob=NULL,n_nodes=N
       return(output_matrix)
     } else {
       if(is.null(n_nodes)){n_nodes<-ncol(bin_data_df)/2}
-      lm_test_sjob <- rslurm::slurm_apply(function(x,y) -log(summary(lm(unlist(bin_data_df[,y])~unlist(bin_data_df[,x])))$coefficients[2,4]), bin.pairs, jobname = 'test_apply',
-                                  nodes = n_nodes, cpus_per_node = cpus_on_each_node, submit = T,slurm_options = list(partition="ccr,norm,quick",mem=memory_per_node,time=walltime,cpus_per_task=cpus_on_each_node))
+      lm_test_sjob <- rslurm::slurm_apply(function(x,y) -log(summary(lm(unlist(bin_data_df[,y])~unlist(bin_data_df[,x])))$coefficients[2,4]), bin.pairs, jobname = 'CNVScope_LM_apply',
+                                          nodes = n_nodes, 
+                                          cpus_per_node = cpus_on_each_node, 
+                                          submit = T,slurm_options = list(partition=partitions,mem=memory_per_node,time=walltime))
       return(lm_test_sjob)
     }
   }
